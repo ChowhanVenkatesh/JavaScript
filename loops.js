0702c0eefccdbc83c5f9502ba4bfa00086804947
Chowhan Venkatesh <myselfvenkatesh9@gmail.com>
2025-01-03 21:39:51 +0530

    loops file added.

---
+++ loops.js
@@ -0,0 +1,22 @@
+for ( let i=1; i <= 10; i++) {
+    console.log("vishal pandey");
+}
+
+//print sum of 1 to 10 
+{
+    let sum = 0;
+for (let i=1; i<=5; i++){
+    sum = sum + i;
+}
+console.log(sum);
+}
+
+//taking input from user 
+{
+    let sum = 0;
+    let num = prompt("Enter your last number = ");
+for (let i=1; i<=num; i++){
+    sum = sum + i;
+}
+console.log(sum);
+}
