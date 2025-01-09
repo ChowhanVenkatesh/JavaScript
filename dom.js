9d422e016de662b9ec50bb07bb15c69e324eb104
Chowhan Venkatesh <myselfvenkatesh9@gmail.com>
2025-01-09 15:18:52 +0530

    DOMs Practice Problems

--- dom.js
+++ dom.js
@@ -88,3 +88,86 @@
     //commonText[1].innerText = "new value 2";
     //commonText[2].innerText = "new value 3";
 }
+
+
+let div = document.querySelector("div");
+console.log(div);
+
+let val = div.getAttribute("id");
+console.log(val); //getAttribute Value
+
+let disco = document.querySelector("p");
+console.dir(disco);
+
+let valll = disco.getAttribute("class");
+console.log(valll);
+
+let discoo = disco.setAttribute("class", "myNewValue");
+console.dir(discoo); //setAttribute Value 
+
+let stylingdiv = document.querySelector("div");
+console.dir(stylingdiv);
+console.log(stylingdiv);
+
+//let stylingbox = stylingdiv.style.backgroundColor = "orange";
+let stylingbox = stylingdiv.style.fontFamily = "Roboto";
+//let stylingbox = stylingdiv.style.borderRadius = "10px";
+//let stylingbox = stylingdiv.style.color = "blue";
+console.dir(stylingbox);
+console.log(stylingbox); // node styling example
+
+let newBtn = document.createElement("button");
+newBtn.innerText = "Click me!";
+console.log(newBtn.style.backgroundColor ="orange"); //creating new element
+
+let divs = document.querySelector("div");
+//divs.append(newBtn);
+//divs.prepend(newBtn);
+//divs.before(newBtn);
+//divs.after(newBtn); //adding new element
+newBtn.remove();
+console.log(divs);
+
+let newBtn2 = document.createElement("button");
+newBtn2.innerText = "Click me! ";
+console.log(newBtn2.style.margin = "5px"); //creating new element
+
+
+let divss = document.querySelector("p");
+//divss.append(newBtn2);
+//divss.prepend(newBtn2);
+//divss.before(newBtn2); //adding new element
+//divss.after(newBtn2);
+newBtn2.remove(); //delete element
+console.log(divss);
+
+let newHeading = document.createElement("h1");
+newHeading.innerHTML = "<i>I am New Here! </i>"; //inserting new heading at the top of the page.
+
+document.querySelector("body").prepend(newHeading);
+
+{
+    /*Practice Q1: Create a new button element.
+    Give it a text “click me”, background color of red & text color of white.
+    Insert the button as the first element inside the body tag.*/
+
+    let createButton = document.createElement("button");
+    createButton.innerText = "Click me!";
+    createButton.style.color = "white";
+    createButton.style.backgroundColor = "red";
+
+    document.querySelector("body").prepend(createButton);
+}
+
+{
+    /*Practice Q2: Create a <p> tag in html, give it a class & some styling.
+    Now create a new class in CSS and try to append this class to the <p> element.
+    Solve this problem using classList.
+    Did you notice, how you overwrite the class name when you add a new one? */
+    let newSol = document.querySelector("p");
+    console.log(newSol);
+    
+    //let newMethod = newSol.classList.add("newContent");
+    let newMethod = newSol.classList.remove("newContent");
+    console.log(newMethod);
+}
