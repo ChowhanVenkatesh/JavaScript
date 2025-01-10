ef6acf99293ad564f4b52cff378b2eec677b1fd7
Chowhan Venkatesh <myselfvenkatesh9@gmail.com>
2025-01-10 13:51:38 +0530

    events practice problem updated

--- events.js
+++ events.js
@@ -42,4 +42,25 @@ btn2.addEventListener("click", (e) => {
     console.log("button2.4");
 });
 
-btn2.removeEventListener("click", handler);
+btn2.removeEventListener("click", handler); //removing evenlisteners.
+
+
+    /*Practice Q: Create a toggle button that changes the screen to dark-mode when clicked & light-mode
+    when clicked again */
+    let modeBtn = document.querySelector("#mode");
+    let body = document.querySelector("body");
+    let currMode = "light";
+
+    modeBtn.addEventListener("click", () => {
+        if (currMode === "light") {
+            currMode = "dark";
+            body.classList.add("dark");
+            body.classList.remove("light");
+        } else {
+            currMode = "light";
+            body.classList.add("light");
+            body.classList.remove("dark");
+        }
+
+        console.log(currMode);
+    });
