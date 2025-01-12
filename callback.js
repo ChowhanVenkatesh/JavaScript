959bc7db1ea0026b2efec5cab23e7b04ba2c1313
Chowhan Venkatesh <myselfvenkatesh9@gmail.com>
2025-01-13 01:14:44 +0530

    callback file updated

--- callback.js
+++ callback.js
@@ -17,3 +17,41 @@ setTimeout (() => {
 console.log("four"); //if we add synchronous statements after asynchronous statement, first synchronous statements executes. 
 console.log("five");
 console.log("six");
+
+//Callbacks
+function sum (a,b) {
+    console.log(a+b);
+}
+
+function calculator (a,b, sumCallback) {
+    sumCallback(a,b);
+}
+calculator(5,9,sum);
+
+
+//setTimeout
+function getData (dataId, getNextData) {
+    setTimeout(() => {
+        console.log("data", dataId);
+        if (getNextData){
+            getNextData();
+        }
+    }, 2000);
+}
+
+//callback hell
+getData(1, () => {
+    getData (2, () => {
+        getData (3, () => {
+            getData (4, () => {
+
+            });
+        });
+    });
+});
+
+let promise = new Promise((resolve, reject) => {
+    console.log("I am a promise");
+    //resolve = "success";
+    reject = "some error";
+});
