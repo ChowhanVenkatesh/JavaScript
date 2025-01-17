8b291ee4d844fa5980c04a9343d98214d7943ae5
Chowhan Venkatesh <myselfvenkatesh9@gmail.com>
2025-01-17 10:31:26 +0530

    callback file updated

--- callback.js
+++ callback.js
@@ -30,28 +30,86 @@ calculator(5,9,sum);
 
 
 //setTimeout
-function getData (dataId, getNextData) {
-    setTimeout(() => {
-        console.log("data", dataId);
-        if (getNextData){
-            getNextData();
-        }
-    }, 2000);
-}
+//function getData (dataId, getNextData) {
+   // setTimeout(() => {
+    //    console.log("data", dataId);
+    //    if (getNextData){
+    //        getNextData();
+    //    }
+   // }, 2000);
+//}
 
 //callback hell
-getData(1, () => {
-    getData (2, () => {
-        getData (3, () => {
-            getData (4, () => {
+//getData(1, () => {
+  //  getData (2, () => {
+    //    getData (3, () => {
+        //    getData (4, () => {
 
-            });
-        });
-    });
-});
+        //    });
+        //});
+    //});
+//});
 
-let promise = new Promise((resolve, reject) => {
-    console.log("I am a promise");
+
+//promise little bit confusing concept.
+//let promise = new Promise((resolve, reject) => {
+  //  console.log("I am a promise");
     //resolve = "success";
-    reject = "some error";
+    //reject = "some error";
+//});
+
+//const getPromise = () => {
+  //  return new Promise((resolve, reject) => {
+    //    console.log("I am a promise");
+     //   resolve("success");
+        //reject("some error");
+    //});
+//}
+
+//let promise = getPromise();
+//promise.then((res) => {
+   // console.log("promise fullfilled", res);
+//});
+
+//promise.catch((err) => {
+   // console.log("rejected", err);
+//});
+
+//function getData (dataId, getNextData) {
+  //  return new Promise ((resolve, reject) => {
+    //    setTimeout(() => {
+            //console.log("data", dataId);
+            //resolve("success");
+      //      reject("error");
+        //    if (getNextData){
+          //      getNextData();
+            //}
+       // }, 5000);
+    //});
+//}
+
+function asyncFunc1 () {
+    return new Promise((resolve, reject ) => {
+        setTimeout(() => {
+            console.log("data1");
+            resolve("success");
+        }, 3000);
+    });
+}
+
+function asyncFunc2 () {
+    return new Promise((resolve, reject ) => {
+        setTimeout(() => {
+            console.log("data2");
+            resolve("success");
+        }, 6000);
+    });
+}
+
+console.log("searching data1");
+let p1 = asyncFunc1();
+p1.then((res) => {
+    console.log(res);
+    console.log("searching data2");
+    let p2 = asyncFunc2((res) => {});
 });
