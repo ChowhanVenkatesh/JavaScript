211640507fe1595feba7709de4ed29cfd680d03b
Chowhan Venkatesh <myselfvenkatesh9@gmail.com>
2025-01-17 17:39:27 +0530

    callbacks completed

--- callback.js
+++ callback.js
@@ -51,7 +51,6 @@ calculator(5,9,sum);
 //});
 
 
-//promise little bit confusing concept.
 //let promise = new Promise((resolve, reject) => {
   //  console.log("I am a promise");
     //resolve = "success";
@@ -75,19 +74,7 @@ calculator(5,9,sum);
    // console.log("rejected", err);
 //});
 
-//function getData (dataId, getNextData) {
-  //  return new Promise ((resolve, reject) => {
-    //    setTimeout(() => {
-            //console.log("data", dataId);
-            //resolve("success");
-      //      reject("error");
-        //    if (getNextData){
-          //      getNextData();
-            //}
-       // }, 5000);
-    //});
-//}
-
+//promise-chain 
 function asyncFunc1 () {
     return new Promise((resolve, reject ) => {
         setTimeout(() => {
@@ -113,3 +100,63 @@ p1.then((res) => {
     console.log("searching data2");
     let p2 = asyncFunc2((res) => {});
 });
+
+
+
+//async function
+async function myFunc() {
+    console.log("Hello World!");
+};
+
+myFunc();
+
+function api () {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            console.log("weather data");
+            resolve(200);
+        });
+    });
+};
+
+async function getWeatherData() {
+    await api();
+    await api();
+};
+
+getWeatherData();
+
+function getData (dataId) {
+    return new Promise((resolve, reject) => {
+        setTimeout (() => {
+            console.log("Data 1");
+        }, 2000);
+    });
+}
+
+getData(1, () => {
+     getData (2, () => {
+         getData (3, () => {
+              getData (4, () => {
+                console.log("end");  //callbackhell process
+              });
+          });
+      });
+  });
+
+getData(1).then((res) => {
+    return getData(2);   //promise chain process
+}).then((res) => {
+    return getData(3);
+});
+
+
+getData();
+
+
+async function getAllData (){
+    await getData(1);   //async-await process
+    await getData(2);
+};
+
+getAllData();
