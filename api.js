6e61cb674228ca40507d2d70649caf5755eb5e0b
Chowhan Venkatesh <myselfvenkatesh9@gmail.com>
2025-01-18 11:48:27 +0530

    api topic completed

--- api.js
+++ api.js
@@ -1,11 +1,20 @@
-const URL = "https://cat-fact.herokuapp.com/facts";
-let promise = fetch(URL);
-console.log(promise);
+const URL = "https://api.txtlocal.com/command";
+
+const factPara = document.querySelector(".fact");
+const factButton = document.querySelector("#factMode");
+
+//let promise = fetch(URL);
+//console.log(promise);
 
 const getFacts = async () => {
     console.log("getting data ......");
     let response = await fetch(URL);
-    //console.log(response);
-    let data = await response.JSON();
-    console.log(data);
+    console.log(response);
+    let data = await response.json();
+    //console.log(data);
+    factPara.innerText = data ;
 }
+
+getFacts();
+
+factButton.addEventListener("click", getFacts);
